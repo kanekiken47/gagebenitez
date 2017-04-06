1bed2595d07f1a4c562c6931dd28ce9a8dcebd63
Jacqueline <Jacqueline@Jacquelines-MacBook-Air.local>
2017-04-06 13:39:30 -0500

    first commit

---
+++ gagefirstjavascript.js
@@ -0,0 +1,52 @@
+function dosomething() {
+    if (confirm("do you want to proceed to youtube?") == true) 
+    	{window.open('https://www.youtube.com/watch?v=U66dciR-fCY')}
+    else {}
+}
+
+function MOWYPINW() {
+    if (confirm("do you want to proceed to youtube?") == true) 
+    	{window.open('http://www.huffingtonpost.com/2015/05/06/bsl-map_n_7216190.html')}
+    else {}
+}
+
+function HTA() {
+    if (confirm("do you want to proceed to youtube?") == true) 
+    	{window.open('https://www.youtube.com/watch?v=06T-_y1RMiQ')}
+    else {}
+}
+
+function APBT() {
+    if (confirm("do you want to proceed to youtube?") == true) 
+        {window.open('http://www.globalanimal.org/2015/07/10/pit-bull-discrimination-a-people-problem/?gclid=CLD9693O_NICFQa5wAodfMILLA')}
+    else {}
+}
+
+
+document.getElementById("luciusdefaultID").addEventListener("click", displayLucius);
+
+function displayLucius() {
+    document.getElementById("imageholder").src='images/lucius.jpg'
+}
+
+
+
+
+
+
+document.getElementById("luciusballID").addEventListener("click", displayLuciusball);
+
+function displayLuciusball() {
+    document.getElementById("imageholder").src='images/luciusball.jpeg'
+}
+
+
+
+
+
+document.getElementById("luciuscaliID").addEventListener("click", displayLuciuscali);
+
+function displayLuciuscali() {
+    document.getElementById("imageholder").src='images/luciuscali.jpeg'
+}
+
---
+++ untitled folder/test.js
@@ -0,0 +1,30 @@
+
+
+document.getElementById("luciusdefaultID").addEventListener("click", displayLucius);
+
+function displayLucius() {
+    document.getElementById("imageholder").src='images/lucius.jpg'
+}
+
+
+
+
+
+
+document.getElementById("luciusballID").addEventListener("click", displayLuciusball);
+
+function displayLuciusball() {
+    document.getElementById("imageholder").src='images/luciousball.jpeg'
+}
+
+
+
+
+
+document.getElementById("luciuscaliID").addEventListener("click", displayLuciuscali);
+
+function displayLuciuscali() {
+    document.getElementById("imageholder").src='images/luciouscali.jpeg'
+}
+
+
